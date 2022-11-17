63ba9233929de4b82dc71194e3fe925cabc701fb
Tamu-sempai <veronikakiseleva86@gmail.com>
2022-11-17 14:26:45 +0500

    first commit

--- app/src/main/java/com/bignerdranch/android/criminalintel/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintel/CrimeRepository.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.bignerdranch.android.criminalintel.datebase.CrimeDataBase
 import java.util.UUID
+import java.util.concurrent.Executors
 
-private  const val DATABASE_NAME = "crimedatabase"
+
+private  const val DATABASE_NAME = "crime-database"
 class CrimeRepository private constructor(context: Context) {
     private val database : CrimeDataBase = Room.databaseBuilder(
         context.applicationContext,
@@ -14,8 +16,20 @@ class CrimeRepository private constructor(context: Context) {
         DATABASE_NAME
     ).build()
     private  val crimeDao = database.crimeDao()
+    private val executor = Executors.newSingleThreadExecutor()
+
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
     fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+    fun updateCrime(crime: Crime){
+        executor.execute{
+            crimeDao.updateCrime(crime)
+        }
+    }
+    fun addCrime(crime: Crime){
+        executor.execute{
+            crimeDao.updateCrime(crime)
+        }
+    }
     companion object{
         private var INSTANCE: CrimeRepository? = null
         fun initialize(context: Context) {
--- app/src/main/java/com/bignerdranch/android/criminalintel/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintel/MainActivity.kt
@@ -2,14 +2,18 @@ package com.bignerdranch.android.criminalintel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-        if (currentFragment == null){
+        if (currentFragment == null) {
             val fragment = CrimeListFragment.newInstance()
             supportFragmentManager
                 .beginTransaction()
@@ -17,4 +21,16 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
     }
+
+    override fun onCrimeSelected(crimeId: UUID) {
+        val fragment = CrimeFragment.newInstance(crimeId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container,fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
 }
+
+
